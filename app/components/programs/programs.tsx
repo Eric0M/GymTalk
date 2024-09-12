24b5e2467f0781b47a56d17d460ff9164f736c2b
Eric <EricM.personal@gmail.com>
2024-09-12 14:21:31 -0300

    Fixed images so that they load on all devices

--- app/components/programs/programs.tsx
+++ app/components/programs/programs.tsx
@@ -1,28 +1,25 @@
 "use client";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
-import planche from "../../../public/planche.jpg";
-import handstand from "../../../public/Handstand.jpg";
-import front_lever from "../../../public/Front_Lever.png";
 
 export default function ProgramOptions() {
   const programs = [
     {
-      image: planche,
+      image: "/planch.jpg",
       title: "Intermediate to Advanced Calisthenics",
       description:
         "For athletes who have a strong foundation and looking to take their progress to the next level.",
       buttonText: "Get Started",
     },
     {
-      image: handstand,
+      image: "/handstand.jpg",
       title: "Handstand Mastery",
       description:
         "For people both just starting out or advanced athletes looking to improve their technique.",
       buttonText: "Get Started",
     },
     {
-      image: front_lever,
+      image: "/front_lever.png",
       title: "All you need to know about Calisthenics",
       description:
         "This is the perfect all inclusive program for anyone wanting to improve at calisthenics.",
@@ -41,12 +38,14 @@ export default function ProgramOptions() {
                 <Image
                   src={program.image}
                   alt={program.title}
-                  layout="fill"
-                  objectFit="cover"
-                  className="rounded-lg"
+                  fill
+                  sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                  className="rounded-lg object-cover"
                 />
               </div>
-              <h3 className="text-xl font-bold mb-2">{program.title}</h3>
+              <h3 className="text-xl font-bold mb-2 text-center">
+                {program.title}
+              </h3>
               <p className="text-center mb-4 text-gray-400">
                 {program.description}
               </p>
