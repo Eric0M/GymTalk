3d449c823dfcda375f7ac4454c21363febb498be
Eric <EricM.personal@gmail.com>
2024-09-18 14:58:05 -0300

    j

--- components/AuthProvider.tsx
+++ components/AuthProvider.tsx
@@ -2,12 +2,7 @@
 
 import { SessionProvider } from "next-auth/react";
 
-const AuthProvider = ({
-  children,
-}: {
-  children: React.ReactNode;
-  session: any;
-}) => {
+const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   return <SessionProvider>{children}</SessionProvider>;
 };
 
