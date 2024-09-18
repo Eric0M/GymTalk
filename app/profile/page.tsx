96f3770355813936b9bd7d791802c7641a74e2ad
Eric <EricM.personal@gmail.com>
2024-09-18 13:43:53 -0300

    More oAuth

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,7 +1,15 @@
+import { getServerSession } from "next-auth";
 import React from "react";
+import { options } from "../api/auth/[...nextauth]/options";
+import { redirect } from "next/navigation";
 
-const page = () => {
-  return <div>page</div>;
+const profile = async () => {
+  const session = await getServerSession(options);
+
+  if (!session) {
+    redirect("/api/auth/signin?callbackUrl=/profile");
+  }
+  return <div>{session?.user?.email}</div>;
 };
 
-export default page;
+export default profile;
