ff5c8f35a8bcb9783eb03363fd0b79a470b75f96
Eric <EricM.personal@gmail.com>
2024-09-18 15:03:01 -0300

    d

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -9,7 +9,7 @@ const profile = async () => {
   if (!session) {
     redirect("/api/auth/signin?callbackUrl=/profile");
   }
-  return <div>{session?.user}</div>;
+  return <div>{session?.user?.email}</div>;
 };
 
 export default profile;
