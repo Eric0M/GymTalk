c87e8f394c3318dd386ab954647dd1f125dbe3e7
Eric <EricM.personal@gmail.com>
2024-09-20 11:51:48 -0300

    edited success page

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
-export const page = () => {
-  return <div>success</div>;
+const SuccessPage = () => {
+  return <div>SuccessPage</div>;
 };
+
+export default SuccessPage;
