4f4faa2502dd0eed00e59bbb1617f9c64dd74962
Eric <EricM.personal@gmail.com>
2024-09-20 22:11:03 -0300

    d

--- app/membership/page.tsx
+++ app/membership/page.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import MembershipTiers from "../disc_standard_and_premium/discord_standard_and_premium";
+import Membership_Tiers from "../disc_standard_and_premium/discord_standard_and_premium";
 
 const page = () => {
   return (
     <>
-      <MembershipTiers />
+      <Membership_Tiers />
     </>
   );
 };
