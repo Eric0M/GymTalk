a33d41e07831f20597305b971599aa7f2d2b29bc
Eric <EricM.personal@gmail.com>
2024-09-22 11:13:26 -0300

    Fiex

--- constants.ts
+++ constants.ts
@@ -3,7 +3,7 @@ export const constants = {
     TestLink:
       process.env.NODE_ENV === "development"
         ? "https://buy.stripe.com/test_14k8zJc0MdKI7CM289"
-        : "",
+        : "https://buy.stripe.com/test_14k8zJc0MdKI7CM289",
     PriceID: "price_1Q19AeIxXY4kjgHfBYnIX8E2",
     Price: 9.99,
     Name: "Basic",
@@ -12,7 +12,7 @@ export const constants = {
     TestLink:
       process.env.NODE_ENV === "development"
         ? "https://buy.stripe.com/test_6oE7vFaWIfSQ2is288"
-        : "",
+        : "https://buy.stripe.com/test_6oE7vFaWIfSQ2is288",
     PriceID: "price_1Q19AeIxXY4kjgHfBYnIX8E2",
     Price: 19.99,
     Name: "Pro",
