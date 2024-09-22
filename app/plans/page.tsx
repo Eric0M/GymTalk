3a463bff0e25f2f7b78a75e176fea0902fb94b5b
Eric <EricM.personal@gmail.com>
2024-09-22 11:10:17 -0300

    f

--- app/plans/page.tsx
+++ app/plans/page.tsx
@@ -1,14 +1,7 @@
 import PricingComponent from "@/app/components/pricing/pricing";
 import React from "react";
-import { getServerSession } from "next-auth";
-import { options } from "../api/auth/[...nextauth]/options";
-import { redirect } from "next/navigation";
 
-const page = async function () {
-  const session = await getServerSession(options);
-  if (!session) {
-    redirect("/api/auth/signin?callbackUrl=/api/auth/signin");
-  }
+const page = function () {
   return <PricingComponent />;
 };
 
