db2fd59d6158af5d9c8fa2a1dd312841668c8f81
Eric <EricM.personal@gmail.com>
2024-09-22 11:26:05 -0300

    fixed edits

--- constants.ts
+++ constants.ts
@@ -12,7 +12,7 @@ export const constants = {
     TestLink:
       process.env.NODE_ENV === "development"
         ? "https://buy.stripe.com/test_6oE7vFaWIfSQ2is288"
-        : "https://buy.stripe.com/test_6oE7vFaWIfSQ2is288",
+        : "https://buy.stripe.com/test_14k8zJc0MdKI7CM289",
     PriceID: "price_1Q19AeIxXY4kjgHfBYnIX8E2",
     Price: 19.99,
     Name: "Pro",
