26c2eca3c25e9f3197cc662a0967f33df08bdde4
Eric <EricM.personal@gmail.com>
2024-09-22 11:08:22 -0300

    edit?

--- app/membership/page.tsx
+++ app/membership/page.tsx
@@ -1,14 +1,7 @@
 import React from "react";
 import Membership_Tiers from "../components/disc_standard_and_premium/discord_standard_and_premium";
-import { getServerSession } from "next-auth";
-import { options } from "../api/auth/[...nextauth]/options";
-import { redirect } from "next/navigation";
 
-const page = async function () {
-  const session = await getServerSession(options);
-  if (!session) {
-    redirect("/api/auth/signin?callbackUrl=/api/auth/signin");
-  }
+const page = function () {
   return <Membership_Tiers />;
 };
 
--- app/plans/page.tsx
+++ app/plans/page.tsx
@@ -1,7 +1,14 @@
 import PricingComponent from "@/app/components/pricing/pricing";
 import React from "react";
+import { getServerSession } from "next-auth";
+import { options } from "../api/auth/[...nextauth]/options";
+import { redirect } from "next/navigation";
 
-const page = () => {
+const page = async function () {
+  const session = await getServerSession(options);
+  if (!session) {
+    redirect("/api/auth/signin?callbackUrl=/api/auth/signin");
+  }
   return <PricingComponent />;
 };
 
