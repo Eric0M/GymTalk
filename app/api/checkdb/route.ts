cfdc9ea0aecfd04addabd9cdf9e32c513b7e79e3
Eric <EricM.personal@gmail.com>
2024-09-23 11:24:54 -0300

    deleted error

--- app/api/checkdb/route.ts
+++
@@ -1,7 +0,0 @@
-// import { connectToDB } from "@/app/utils/database";
-// import { NextResponse } from "next/server";
-
-// export async function GET() {
-//   const con = await connectToDB();
-//   return new NextResponse("Connected to MongoDB");
-// }
