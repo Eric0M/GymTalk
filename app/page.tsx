15cb3e0fa068f3d5b90263479def7e6c1633dd14
Eric <EricM.personal@gmail.com>
2024-09-25 08:14:34 -0300

    added to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,6 @@
-import Header from "./components/header/header";
-import About from "./components/about_me/about_me";
-import Product_Card from "./components/product_card/product_card";
-import Pricing from "./components/pricing/pricing";
 import Why_us from "./components/why_us/why_us";
 import Hero from "./components/test_hero/test_hero";
 import ProgramOptions from "./components/programs/programs";
-import Discord from "./components/discord_invite/discord_invite";
 import Disc_standard_and_premium from "./components/disc_standard_and_premium/discord_standard_and_premium";
 
 export default function Home() {
@@ -13,6 +8,7 @@ export default function Home() {
     <>
       <Hero />
       <Why_us />
+      <Disc_standard_and_premium />
       <ProgramOptions />
     </>
   );
