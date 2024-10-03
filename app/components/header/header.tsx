657e2033f2c00284a1b7b4f3b80f8f1ba15a982a
Eric <EricM.personal@gmail.com>
2024-10-03 12:37:52 -0300

    removed mobile menu from old header

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -60,7 +60,6 @@ export default async function Header() {
               </Link>
             )}
           </div>
-          <MobileMenu navItems={navItems} />
         </div>
       </div>
     </header>
